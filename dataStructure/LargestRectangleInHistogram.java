fae98b51e9f95a2bd72ae2f95909b05e3ab0c4e8
autoload <383756034@qq.com>
2020-12-17 13:48:19 +0800

    add: dataStructure algorithms

--- dataStructure/LargestRectangleInHistogram.java
+++ dataStructure/LargestRectangleInHistogram.java
@@ -1,3 +1,57 @@
 public class LargestRectangleInHistogram {
-    
+    public static void main(String[] args) {
+        int[] nums = {2, 1, 5, 6, 2, 3};   
+        int point = 2;
+        
+        int height = maxRectangle(nums);
+        System.out.println(height);
+    }
+
+    public static int maxRectangle(int[] nums) {
+        int i = 0;
+        int max = 0;
+        int len = nums.length;
+        int heigh = 0;
+        while(i < len) {
+            heigh = findLeft(nums, i) + nums[i] + findRight(nums,i);; 
+            if(heigh > max) {
+                max = heigh;
+            }
+            i++;
+        }
+        return max;
+    }
+
+    public static int findLeft(int[] nums, int point) {
+        int len = nums.length;
+        if(point <= 0  || point == len - 1) {
+            return 0;
+        }
+        int mid = point;
+        int s = 1;
+        while(point > 0 && nums[mid] <= nums[point]) {
+            point--;
+            if(point == 0) {
+                s = 0;
+            }
+        }
+        return (mid - point - s) * nums[mid];
+
+    }
+
+    public static int findRight(int[] nums, int point) {
+        int len = nums.length;
+        if(point <=0  || point == len - 1) {
+            return 0;
+        }
+        int mid = point;
+        int s = 1;
+        while(point < len && nums[mid] <= nums[point]) {
+            point++;
+
+        }
+        return (point - mid - s) * nums[mid];
+    } 
+
+
 }
---
+++ dataStructure/LongestConsecutiveSequence.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        int[] nums = {0,3,7,2,5,8,4,6,0,1};
+        Set<Integer> num_set = new HashSet<Integer>();
+        for (int num : nums) {
+            num_set.add(num);
+        }
+
+        int longestStreak = 0;
+
+        for (int num : num_set) {
+            if (!num_set.contains(num - 1)) {
+                int currentNum = num;
+                int currentStreak = 1;
+
+                while (num_set.contains(currentNum + 1)) {
+                    currentNum += 1;
+                    currentStreak += 1;
+                }
+
+                longestStreak = Math.max(longestStreak, currentStreak);
+            }
+        }
+
+        System.out.println(longestStreak);
+    }
+}
